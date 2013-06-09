87f261747a9d00efa448854b63804e4b75231f1c
plandevida <plandevida@planes.com>
2013-06-09 20:13:16 +0200

    Initial Commit

---
+++ EjercicioNReinas/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EjercicioNReinas
+//
+//  Created by Daniel on 09/06/13.
+//  Copyright (c) 2013 Daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
