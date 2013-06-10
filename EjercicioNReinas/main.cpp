3f716a11baa78fe5b26d140be6d731ce67169f4f
plandevida <plandevida@planes.com>
2013-06-10 14:57:41 +0200

    Reuelto

--- EjercicioNReinas/main.cpp
+++ EjercicioNReinas/main.cpp
@@ -1,18 +1,53 @@
-//
-//  main.cpp
-//  EjercicioNReinas
-//
-//  Created by Daniel on 09/06/13.
-//  Copyright (c) 2013 Daniel. All rights reserved.
-//
-
 #include <iostream>
+
+using namespace std;
+
+void nReinas(int solucion[], int k, int n);
+bool esValida(int solucion[], int k);
+void tratarSolucion(int solucion[], int n);
+void nReinas(int n);
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    nReinas(8);
+   
     return 0;
 }
+void nReinas(int n) {
+    int solucion[n];
+    nReinas(solucion, 0, n);
+}
 
+void nReinas(int solucion[], int k, int n){
+    
+    for(int i = 0; i < n; i++) {
+        solucion[k] = i;
+        if (esValida(solucion, k)) {
+            if(k == n - 1) {
+                tratarSolucion(solucion, n);
+            } else {
+                nReinas(solucion, k + 1, n);
+            }
+        }
+    }
+}
+
+bool esValida(int solucion[], int k) {
+    bool correcto = true;
+    int i = 0;
+    while (i < k && correcto) {
+        if(solucion[i] == solucion[k] || abs(solucion[k] - solucion[i]) == k - i)
+            correcto = false;
+        
+        i++;
+    }
+    return correcto;
+}
+
+void tratarSolucion(int solucion[], int n) {
+    cout << "Solucion: ";
+    for(int i = 0; i < n; i++)
+        cout << solucion[i] << " ";
+    cout << endl;
+}
